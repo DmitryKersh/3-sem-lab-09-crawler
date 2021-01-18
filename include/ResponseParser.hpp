5ffb616dab8e9b00b65b8c851a1d75a5a4adee0f
dmitrykersh <d.karpukhin@mail.ru>
2021-01-19 00:51:51 +0300

    impl: parser

--- include/ResponseParser.hpp
+++ include/ResponseParser.hpp
@@ -5,4 +5,23 @@
 #ifndef CRAWLER_RESPONSEPARSER_HPP
 #define CRAWLER_RESPONSEPARSER_HPP
 
+#include <iostream>
+#include <set>
+#include <string>
+
+#include <UrlReader.hpp>
+
+struct ParsingResult{
+  std::set<std::string> image_urls;
+  std::set<std::string> child_urls;
+
+  [[nodiscard]] bool operator==(ParsingResult const& other) const {
+    return this == &other || (image_urls == other.image_urls && child_urls == other.child_urls);
+  }
+};
+
+std::ostream& operator<< (std::ostream& os, ParsingResult const& result);
+
+[[nodiscard]] ParsingResult parse_response(http_response_t const& response, bool parse_children);
+
 #endif  // CRAWLER_RESPONSEPARSER_HPP
